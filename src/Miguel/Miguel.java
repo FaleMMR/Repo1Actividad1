bb41f7bc421b1d2e88ffad34616cb16910aa27b1
Miguel Capellan Abad <mca81@outlook.es>
2023-02-06 16:13:12 +0100

    primera subida Miguel

---
+++ src/Miguel/Miguel.java
@@ -0,0 +1,19 @@
+package Miguel;
+
+public class Miguel {
+
+	public static void main(String[] args) {
+		String nombre = "Miguel";
+		
+		escribirNombre(nombre);
+
+	}
+
+	public static void  escribirNombre(String nombre) {
+		
+		System.out.println("Hola, me llamo " + nombre);
+		
+	
+	}
+
+}
