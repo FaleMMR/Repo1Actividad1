070e7aee6270eeae92d787c258ef3d6813f4338a
Miguel Capellan Abad <mca81@outlook.es>
2023-02-06 17:40:54 +0100

    comidas carpeta Dani

--- src/Daniel/Daniel.java
+++ src/Daniel/Daniel.java
@@ -6,6 +6,12 @@ public class Daniel {
 			System.out.println(hobby(1,"Caminar"));
 			System.out.println(hobby(2,"Correr"));
 			System.out.println(hobby(3,"Estudiar"));
+			
+
+            System.out.println(comida(1,"pizza"));
+            System.out.println(comida(2,"pollo"));
+            System.out.println(comida(3,"melon"));
+			
 	}
 	public static String literalNombre(String Nombre) {
 		return "Hola, soy"+Nombre;
@@ -13,4 +19,7 @@ public class Daniel {
 	public static String hobby(int i, String Hobby) {
 		return i+" "+Hobby;
 	}
+	  public static String comida(int i, String Comida) {
+	        return i+" "+Comida;
+	    }
 }
