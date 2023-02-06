201e9354c3acf8cc4d04a3c57a22f09e683a2a62
danivodka5 <datmax1@hotmail.com>
2023-02-06 15:20:25 +0100

    Paquete y Clase Daniel

---
+++ src/Daniel/Daniel.java
@@ -0,0 +1,12 @@
+package Daniel;
+
+public class Daniel {
+
+	public static void main(String[] args) {
+			System.out.println(literalNombre("Daniel"));
+	}
+	public static String literalNombre(String Nombre) {
+		return "Hola, soy"+Nombre;
+	}
+
+}
