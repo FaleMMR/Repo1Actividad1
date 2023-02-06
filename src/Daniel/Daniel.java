265bb55eeb846893ff80b6f2bcb6ce06cc8a2de8
danivodka5 <datmax1@hotmail.com>
2023-02-06 17:37:04 +0100

    Comidas 3 Daniel

--- src/Daniel/Daniel.java
+++ src/Daniel/Daniel.java
@@ -6,11 +6,18 @@ public class Daniel {
 			System.out.println(hobby(1,"Caminar"));
 			System.out.println(hobby(2,"Correr"));
 			System.out.println(hobby(3,"Estudiar"));
+			
+			System.out.println(comida(1,"Arroz"));
+			System.out.println(comida(2,"Esparragos"));
+			System.out.println(comida(3,"Mango"));
 	}
 	public static String literalNombre(String Nombre) {
 		return "Hola, soy"+Nombre;
 	}
 	public static String hobby(int i, String Hobby) {
 		return i+" "+Hobby;
+	}
+	public static String comida(int i, String Comida) {
+		return i+" "+Comida;
 	}
 }
